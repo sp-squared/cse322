b11afdb012f5e36eacc94dc11863455d641984bd
Colin A. Morris <colin.morris.r@gmail.com>
2019-11-12 05:06:57 -0800

    x

---
+++ canvas/pt4/global/a.js
@@ -0,0 +1,10 @@
+<div id = "k"></div>
+<script>
+
+// uses window to show it is defined globally --> more explicit to other programmers
+console.log(window.k);
+// same output
+console.log(k);
+
+
+</script>
